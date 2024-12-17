f69e0497b31c486bcc57a2ee9e07431c6dd4fb81
ogolafreeman <jacktoneokowemba14@gmail.com>
2024-12-17 14:11:50 +0300

    Restructuring

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="mh-footer mh-footer-3">
+    <div class="container">
+        <p class="text-center">&copy; <?php echo date('Y'); ?> Portfolio Website. All Rights Reserved.</p>
+    </div>
+</footer>
+
+<!-- Scripts -->
+<script src="assets/plugins/js/jquery.min.js"></script>
+<script src="assets/plugins/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    // Main index file
+    include 'header.php'; // Includes meta tags and stylesheets
+    include 'navbar.php'; // Includes navigation bar
+    //include 'loader.php'; // Includes the page loader
+?>
+<?php
+    include 'home.php'; // home page
+?>
+
+<?php
+    include 'footer.php'; // Includes footer
+?>
