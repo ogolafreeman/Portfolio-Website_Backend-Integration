f38198000878e1a16be958d5d052b378c77a72ee
ogolafreeman <jacktoneokwemba14@gmail.com>
2025-01-04 09:47:20 +0300

    projects

--- project.php
+++ project.php
@@ -1,134 +1,70 @@
 <?php
-    // Main index file
     include 'header.php'; // Includes meta tags and stylesheets
-    //include 'navbar.php'; // Includes navigation bar
-    //include 'loader.php'; // Includes the page loader
+
+    // Database connection
+    $servername = "localhost";
+    $username = "root"; // Replace with your username
+    $password = ""; // Replace with your password
+    $dbname = "folio"; // Database name
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Fetch projects data
+    $projectsQuery = "SELECT * FROM projects";
+    $projectsResult = $conn->query($projectsQuery);
 ?>
-<!--
-        ===================
-          FEATURE PROJECTS
-        ===================
-        -->
-        <section class="mh-featured-project image-bg featured-img-one">
-            <div class="img-color-overlay">
-                <div class="container">
-                    <div class="row section-separator">
-                        <div class="section-title col-sm-12">
-                            <h3>Featured Projects</h3>
-                        </div>
-                        <div class="col-sm-12">
-                            <div class="mh-single-project-slide-by-side row">
-                                <!-- Project Items -->
-                                <div class="col-sm-12 mh-featured-item">
-                                    <div class="row">
-                                        <div class="col-sm-7">
-                                            <div class="mh-featured-project-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">
-                                                <img src="assets/images/p-2.png" alt="" class="img-fluid">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-5">
-                                            <div class="mh-featured-project-content">
-                                                <h4 class="project-category wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">Web Design</h4>
-                                                <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">Wrap</h2>
-                                                <span class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">Design & Development</span>
-                                                <p class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">Stamp is a clean and elegant Multipurpose Landing Page Template. 
-                                                It will fit perfectly for Startup, Web App or any type of Web Services.
-                                                It has 4 background styles with 6 homepage styles. 6 pre-defined color scheme. 
-                                                All variations are organized separately so you can use / customize the template very easily.</p>
-                                                <a href="#" class="btn btn-fill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">View Details</a>
-                                                <div class="mh-testimonial mh-project-testimonial wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.9s">
-                                                    <blockquote>
-                                    					<q>Excellent Template - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                    				<blockquote>
-                                    					<q>Creative Template - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                    				<blockquote>
-                                    					<q>Organize Code - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                                </div>
-                                            </div>
-                                        </div>
+
+<section class="mh-featured-project image-bg featured-img-one">
+    <div class="img-color-overlay">
+        <div class="container">
+            <div class="row section-separator">
+                <div class="section-title col-sm-12">
+                    <h3>Featured Projects</h3>
+                </div>
+
+                <?php if ($projectsResult->num_rows > 0): ?>
+                    <?php while ($project = $projectsResult->fetch_assoc()): ?>
+                        <div class="col-sm-12 mh-featured-item">
+                            <div class="row">
+                                <div class="col-sm-7">
+                                    <div class="mh-featured-project-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">
+                                        <img src="<?php echo $project['image']; ?>" alt="Project Image" class="img-fluid">
                                     </div>
                                 </div>
-                                <!-- Project Items -->
-                                <div class="col-sm-12 mh-featured-item">
-                                    <div class="row">
-                                        <div class="col-sm-7">
-                                            <div class="mh-featured-project-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">
-                                                <img src="assets/images/p-2.png" alt="" class="img-fluid">
+                                <div class="col-sm-5">
+                                    <div class="mh-featured-project-content">
+                                        <h4 class="project-category wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s"><?php echo $project['category']; ?></h4>
+                                        <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s"><?php echo $project['title']; ?></h2>
+                                        <span class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s"><?php echo $project['type']; ?></span>
+                                        <p class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s"><?php echo $project['description']; ?></p>
+                                        <a href="#" class="btn btn-fill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">View Details</a>
+
+                                        <?php if ($project['testimonial']): ?>
+                                            <div class="mh-testimonial mh-project-testimonial wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.9s">
+                                                <blockquote>
+                                                    <q><?php echo $project['testimonial']; ?></q>
+                                                    <cite>- <?php echo $project['testimonial_author']; ?>, <?php echo $project['testimonial_cite']; ?></cite>
+                                                </blockquote>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-5">
-                                            <div class="mh-featured-project-content">
-                                                <h4 class="project-category wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">Web Design</h4>
-                                                <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">Wrap</h2>
-                                                <span class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">Design & Development</span>
-                                                <p class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">Stamp is a clean and elegant Multipurpose Landing Page Template. 
-                                                It will fit perfectly for Startup, Web App or any type of Web Services.
-                                                It has 4 background styles with 6 homepage styles. 6 pre-defined color scheme. 
-                                                All variations are organized separately so you can use / customize the template very easily.</p>
-                                                <a href="#" class="btn btn-fill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">View Details</a>
-                                                <div class="mh-testimonial mh-project-testimonial wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.9s">
-                                                    <blockquote>
-                                    					<q>Excellent Template - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                    				<blockquote>
-                                    					<q>Creative Template - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                    				<blockquote>
-                                    					<q>Organize Code - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Project Items -->
-                                <div class="col-sm-12 mh-featured-item">
-                                    <div class="row">
-                                        <div class="col-sm-7">
-                                            <div class="mh-featured-project-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">
-                                                <img src="assets/images/p-2.png" alt="" class="img-fluid">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-5">
-                                            <div class="mh-featured-project-content">
-                                                <h4 class="project-category wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">Web Design</h4>
-                                                <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">Wrap</h2>
-                                                <span class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">Design & Development</span>
-                                                <p class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">Stamp is a clean and elegant Multipurpose Landing Page Template. 
-                                                It will fit perfectly for Startup, Web App or any type of Web Services.
-                                                It has 4 background styles with 6 homepage styles. 6 pre-defined color scheme. 
-                                                All variations are organized separately so you can use / customize the template very easily.</p>
-                                                <a href="#" class="btn btn-fill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">View Details</a>
-                                                <div class="mh-testimonial mh-project-testimonial wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.9s">
-                                                    <blockquote>
-                                    					<q>Excellent Template - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                    				<blockquote>
-                                    					<q>Creative Template - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                    				<blockquote>
-                                    					<q>Organize Code - suits my needs perfectly whilst allowing me to learn some new technology first hand.</q>
-                                    					<cite>- Shane Kavanagh</cite>
-                                    				</blockquote>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div> <!-- End: .row -->
-                </div>
+                    <?php endwhile; ?>
+                <?php else: ?>
+                    <p class="text-center">No featured projects available at the moment.</p>
+                <?php endif; ?>
             </div>
-        </section>  
+        </div>
+    </div>
+</section>
+
+<?php
+    $conn->close();
+?>
--- skills.php
+++ skills.php
@@ -1,122 +1,83 @@
-        
 <?php
-    // Main index file
     include 'header.php'; // Includes meta tags and stylesheets
- //   include 'navbar.php'; // Includes navigation bar
-    //include 'loader.php'; // Includes the page loader
+
+    // Database connection
+    $servername = "localhost";
+    $username = "root"; // Replace with your username
+    $password = ""; // Replace with your password
+    $dbname = "folio"; // Database name
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Fetch Technical Skills
+    $techSkillsQuery = "SELECT * FROM technical_skills";
+    $techSkillsResult = $conn->query($techSkillsQuery);
+
+    // Fetch Professional Skills
+    $profSkillsQuery = "SELECT * FROM professional_skills";
+    $profSkillsResult = $conn->query($profSkillsQuery);
 ?>
-     <!--
-        ===================
-           SKILLS
-        ===================
-        -->
-        <section class="mh-skills" id="mh-skills">
-            <div class="home-v-img">
-                <div class="container">
-                    <div class="row section-separator">
-                        <div class="section-title text-center col-sm-12">
-                            <!--<h2>Skills</h2>-->
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <div class="mh-skills-inner">
-                                <div class="mh-professional-skill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
-                                    <h3>Technical Skills</h3>
-                                    <div class="each-skills">
+
+<section class="mh-skills" id="mh-skills">
+    <div class="home-v-img">
+        <div class="container">
+            <div class="row section-separator">
+                <!-- Technical Skills -->
+                <div class="col-sm-12 col-md-6">
+                    <div class="mh-skills-inner">
+                        <div class="mh-professional-skill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
+                            <h3>Technical Skills</h3>
+                            <div class="each-skills">
+                                <?php if ($techSkillsResult->num_rows > 0): ?>
+                                    <?php while ($skill = $techSkillsResult->fetch_assoc()): ?>
                                         <div class="candidatos">
                                             <div class="parcial">
                                                 <div class="info">
-                                                    <div class="nome">Javascript</div>
-                                                    <div class="percentagem-num">86%</div>
+                                                    <div class="nome"><?php echo $skill['skill_name']; ?></div>
+                                                    <div class="percentagem-num"><?php echo $skill['proficiency']; ?>%</div>
                                                 </div>
                                                 <div class="progressBar">
-                                                    <div class="percentagem" style="width: 86%;"></div>
+                                                    <div class="percentagem" style="width: <?php echo $skill['proficiency']; ?>%;"></div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="candidatos">
-                                            <div class="parcial">
-                                                <div class="info">
-                                                    <div class="nome">Java</div>
-                                                    <div class="percentagem-num">46%</div>
-                                                </div>
-                                                <div class="progressBar">
-                                                    <div class="percentagem" style="width: 46%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="candidatos">
-                                            <div class="parcial">
-                                                <div class="info">
-                                                    <div class="nome">Python</div>
-                                                    <div class="percentagem-num">38%</div>
-                                                </div>
-                                                <div class="progressBar">
-                                                    <div class="percentagem" style="width: 38%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="candidatos">
-                                            <div class="parcial">
-                                                <div class="info">
-                                                    <div class="nome">PHP</div>
-                                                    <div class="percentagem-num">17%</div>
-                                                </div>
-                                                <div class="progressBar">
-                                                    <div class="percentagem" style="width: 17%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>                                    
-                                        
-                                        <div class="candidatos">
-                                            <div class="parcial">
-                                                <div class="info">
-                                                    <div class="nome">Python</div>
-                                                    <div class="percentagem-num">38%</div>
-                                                </div>
-                                                <div class="progressBar">
-                                                    <div class="percentagem" style="width: 38%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="candidatos">
-                                            <div class="parcial">
-                                                <div class="info">
-                                                    <div class="nome">PHP</div>
-                                                    <div class="percentagem-num">17%</div>
-                                                </div>
-                                                <div class="progressBar">
-                                                    <div class="percentagem" style="width: 17%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <div class="mh-professional-skills wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
-                                <h3>Professional Skills</h3>
-                                <ul class="mh-professional-progress">
-                                    <li>
-                                        <div class="mh-progress mh-progress-circle" data-progress="95"></div>
-                                        <div class="pr-skill-name">Communication</div>
-                                    </li>
-                                    <li>
-                                        <div class="mh-progress mh-progress-circle" data-progress="55"></div> 
-                                        <div class="pr-skill-name">Team Work</div>
-                                    </li>
-                                    <li>
-                                        <div class="mh-progress mh-progress-circle" data-progress="86"></div>
-                                        <div class="pr-skill-name">Project Management</div>
-                                    </li> 
-                                    <li>
-                                        <div class="mh-progress mh-progress-circle" data-progress="60"></div>
-                                        <div class="pr-skill-name">Creativity</div>
-                                    </li>
-                                </ul>
+                                    <?php endwhile; ?>
+                                <?php else: ?>
+                                    <p>No technical skills available.</p>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <!-- Professional Skills -->
+                <div class="col-sm-12 col-md-6">
+                    <div class="mh-professional-skills wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
+                        <h3>Professional Skills</h3>
+                        <ul class="mh-professional-progress">
+                            <?php if ($profSkillsResult->num_rows > 0): ?>
+                                <?php while ($skill = $profSkillsResult->fetch_assoc()): ?>
+                                    <li>
+                                        <div class="mh-progress mh-progress-circle" data-progress="<?php echo $skill['proficiency']; ?>"></div>
+                                        <div class="pr-skill-name"><?php echo $skill['skill_name']; ?></div>
+                                    </li>
+                                <?php endwhile; ?>
+                            <?php else: ?>
+                                <p>No professional skills available.</p>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
+
+<?php
+    $conn->close();
+?>
