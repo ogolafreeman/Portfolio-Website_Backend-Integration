37e05820ab270a33fb24f0ada9359495df856900
ogolafreeman <jacktoneokwemba14@gmail.com>
2025-01-07 01:01:51 +0300

    font-icons

--- admin/view_projects.php
+++ admin/view_projects.php
@@ -1,46 +1,66 @@
 <?php
-    include 'header.php'; // Includes admin panel header
-    
-
-    // Database connection
-    include 'config.php';
-
+include 'header.php'; // Includes admin panel header
+include 'config.php'; // Includes database connection
 ?>
+
 <div class="container-fluid">
     <div class="row">
-            <?php include('sidebar.php'); ?>
+        <?php include('sidebar.php'); // Includes the sidebar ?>
+        
         <div class="col-9 p-4">
-        <table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>Category</th>
-            <th>Title</th>
-            <th>Type</th>
-            <th>Description</th>
-            <th>Image</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $projectsQuery = "SELECT * FROM projects";
-        $projectsResult = $conn->query($projectsQuery);
+            <div class="card shadow-lg">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="mb-0">Projects List</h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped table-hover align-middle">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>Category</th>
+                                <th>Title</th>
+                                <th>Type</th>
+                                <th>Description</th>
+                                <th>Image</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $projectsQuery = "SELECT * FROM projects";
+                            $projectsResult = $conn->query($projectsQuery);
 
-        while ($row = $projectsResult->fetch_assoc()) {
-            echo "<tr>
-                    <td>{$row['category']}</td>
-                    <td>{$row['title']}</td>
-                    <td>{$row['type']}</td>
-                    <td>{$row['description']}</td>
-                    <td><img src='{$row['image']}' alt='Project Image' style='width:100px;'></td>
-                    <td>
-                        <a href='edit_project.php?id={$row['id']}' class='btn btn-warning btn-sm'>Edit</a>
-                        <a href='delete_project.php?id={$row['id']}' class='btn btn-danger btn-sm' onclick=\"return confirm('Are you sure you want to delete this project?');\">Delete</a>
-                    </td>
-                  </tr>";
-        }
-        ?>
-    </tbody>
-</table>
-</div>
+                            if ($projectsResult->num_rows > 0) {
+                                while ($row = $projectsResult->fetch_assoc()) {
+                                    echo "
+                                    <tr>
+                                        <td>{$row['category']}</td>
+                                        <td>{$row['title']}</td>
+                                        <td>{$row['type']}</td>
+                                        <td>" . (strlen($row['description']) > 50 ? substr($row['description'], 0, 50) . '...' : $row['description']) . "</td>
+                                        <td>
+                                            <img src='{$row['image']}' alt='Project Image' class='img-thumbnail' style='width: 100px; height: auto;'>
+                                        </td>
+                                        <td>
+                                            <a href='edit_project.php?id={$row['id']}' class='btn btn-warning btn-sm'>
+                                                <i class='fa fa-edit'></i> Edit
+                                            </a>
+                                            <a href='delete_project.php?id={$row['id']}' class='btn btn-danger btn-sm' onclick=\"return confirm('Are you sure you want to delete this project?');\">
+                                                <i class='fa fa-trash'></i> Delete
+                                            </a>
+                                        </td>
+                                    </tr>";
+                                }
+                            } else {
+                                echo "
+                                <tr>
+                                    <td colspan='6' class='text-center text-muted'>No projects found.</td>
+                                </tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- admin/view_skills.php
+++ admin/view_skills.php
@@ -1,75 +1,101 @@
 <?php
-    include 'header.php'; // Includes admin panel header
-    
-
-    // Database connection
-    include 'config.php';
-
+include 'header.php'; // Includes admin panel header
+include 'config.php'; // Includes database connection
 ?>
 <div class="container-fluid">
     <div class="row">
-            <?php include('sidebar.php'); ?>
+        <?php include('sidebar.php'); // Includes the sidebar ?>
         <div class="col-9 p-4">
-        <h2>View Skills</h2>
-</br>
- <!-- Display Technical Skills -->
- <h4>Technical Skills</h4>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Skill Name</th>
-                <th>Proficiency (%)</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $techSkillsQuery = "SELECT * FROM technical_skills";
-            $techSkillsResult = $conn->query($techSkillsQuery);
+            <div class="card shadow-lg">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="mb-0">View Skills</h3>
+                </div>
+                <div class="card-body">
+                    <!-- Display Technical Skills -->
+                    <h4 class="mb-4">Technical Skills</h4>
+                    <table class="table table-striped table-hover align-middle">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>Skill Name</th>
+                                <th>Proficiency (%)</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $techSkillsQuery = "SELECT * FROM technical_skills";
+                            $techSkillsResult = $conn->query($techSkillsQuery);
 
-            while ($row = $techSkillsResult->fetch_assoc()) {
-                echo "<tr>
-                        <td>{$row['skill_name']}</td>
-                        <td>{$row['proficiency']}%</td>
-                        <td>
-                            <a href='edit_skill.php?type=technical&id={$row['id']}' class='btn btn-sm btn-warning'>Edit</a>
-                            <a href='delete_skill.php?type=technical&id={$row['id']}' class='btn btn-sm btn-danger' onclick=\"return confirm('Are you sure you want to delete this skill?');\">Delete</a>
-                        </td>
-                      </tr>";
-            }
-            ?>
-        </tbody>
-    </table>
-     <!-- Display Professional Skills -->
-     <h4>Professional Skills</h4>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Skill Name</th>
-                <th>Proficiency (%)</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $profSkillsQuery = "SELECT * FROM professional_skills";
-            $profSkillsResult = $conn->query($profSkillsQuery);
+                            if ($techSkillsResult->num_rows > 0) {
+                                while ($row = $techSkillsResult->fetch_assoc()) {
+                                    echo "
+                                    <tr>
+                                        <td>{$row['skill_name']}</td>
+                                        <td>{$row['proficiency']}%</td>
+                                        <td>
+                                            <a href='edit_skill.php?type=technical&id={$row['id']}' class='btn btn-warning btn-sm'>
+                                                <i class='fa fa-edit'></i> Edit
+                                            </a>
+                                            <a href='delete_skill.php?type=technical&id={$row['id']}' class='btn btn-danger btn-sm' onclick=\"return confirm('Are you sure you want to delete this skill?');\">
+                                                <i class='fa fa-trash'></i> Delete
+                                            </a>
+                                        </td>
+                                    </tr>";
+                                }
+                            } else {
+                                echo "
+                                <tr>
+                                    <td colspan='3' class='text-center text-muted'>No technical skills found.</td>
+                                </tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
 
-            while ($row = $profSkillsResult->fetch_assoc()) {
-                echo "<tr>
-                        <td>{$row['skill_name']}</td>
-                        <td>{$row['proficiency']}%</td>
-                        <td>
-                            <a href='edit_skill.php?type=professional&id={$row['id']}' class='btn btn-sm btn-warning'>Edit</a>
-                            <a href='delete_skill.php?type=professional&id={$row['id']}' class='btn btn-sm btn-danger' onclick=\"return confirm('Are you sure you want to delete this skill?');\">Delete</a>
-                        </td>
-                      </tr>";
-            }
-            ?>
-        </tbody>
-    </table>
+                    <!-- Display Professional Skills -->
+                    <h4 class="mt-5 mb-4">Professional Skills</h4>
+                    <table class="table table-striped table-hover align-middle">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>Skill Name</th>
+                                <th>Proficiency (%)</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $profSkillsQuery = "SELECT * FROM professional_skills";
+                            $profSkillsResult = $conn->query($profSkillsQuery);
 
-
+                            if ($profSkillsResult->num_rows > 0) {
+                                while ($row = $profSkillsResult->fetch_assoc()) {
+                                    echo "
+                                    <tr>
+                                        <td>{$row['skill_name']}</td>
+                                        <td>{$row['proficiency']}%</td>
+                                        <td>
+                                            <a href='edit_skill.php?type=professional&id={$row['id']}' class='btn btn-warning btn-sm'>
+                                                <i class='fa fa-edit'></i> Edit
+                                            </a>
+                                            <a href='delete_skill.php?type=professional&id={$row['id']}' class='btn btn-danger btn-sm' onclick=\"return confirm('Are you sure you want to delete this skill?');\">
+                                                <i class='fa fa-trash'></i> Delete
+                                            </a>
+                                        </td>
+                                    </tr>";
+                                }
+                            } else {
+                                echo "
+                                <tr>
+                                    <td colspan='3' class='text-center text-muted'>No professional skills found.</td>
+                                </tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php include('footer.php'); ?>
