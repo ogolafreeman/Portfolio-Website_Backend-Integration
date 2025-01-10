d738bd3bba15848de0e57898beca8777660b51a6
ogolafreeman <jacktoneokwemba14@gmail.com>
2025-01-10 12:58:36 +0300

    home circle border

--- home.php
+++ home.php
@@ -19,6 +19,7 @@ if ($result->num_rows > 0) {
     $twitter = $homeData['twitter_link'];
     $github = $homeData['github_link'];
     $dribbble = $homeData['dribbble_link'];
+    $image = "admin/" . $homeData['image']; // Adding the correct path to the image
 } else {
     echo "No data found in the database!";
 }
@@ -39,36 +40,36 @@ $conn->close();
                             <span>Hello I'm</span>
                         </div>
                         
-                        <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"><?php echo $name; ?></h2>
-                        <h4 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s"><?php echo $title; ?></h4>
+                        <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"><?php echo htmlspecialchars($name); ?></h2>
+                        <h4 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s"><?php echo htmlspecialchars($title); ?></h4>
                         
                         <ul>
                             <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
                                 <i class="fa fa-envelope"></i>
-                                <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+                                <a href="mailto:<?php echo htmlspecialchars($email); ?>"><?php echo htmlspecialchars($email); ?></a>
                             </li>
                             <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
                                 <i class="fa fa-phone"></i>
-                                <a href="callto:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+                                <a href="tel:<?php echo htmlspecialchars($phone); ?>"><?php echo htmlspecialchars($phone); ?></a>
                             </li>
                             <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
                                 <i class="fa fa-map-marker"></i>
-                                <address><?php echo $address; ?></address>
+                                <address><?php echo htmlspecialchars($address); ?></address>
                             </li>
                         </ul>
                         
                         <ul class="social-icon wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">
-                            <li><a href="<?php echo $facebook; ?>"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="<?php echo $twitter; ?>"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="<?php echo $github; ?>"><i class="fa fa-github"></i></a></li>
-                            <li><a href="<?php echo $dribbble; ?>"><i class="fa fa-dribbble"></i></a></li>
+                            <li><a href="<?php echo htmlspecialchars($facebook); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="<?php echo htmlspecialchars($twitter); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="<?php echo htmlspecialchars($github); ?>" target="_blank"><i class="fa fa-github"></i></a></li>
+                            <li><a href="<?php echo htmlspecialchars($dribbble); ?>" target="_blank"><i class="fa fa-dribbble"></i></a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="hero-img wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
                         <div class="img-border">
-                            <img src="assets/images/hero.png" alt="Hero Image" class="img-fluid">
+                            <img src="<?php echo htmlspecialchars($image); ?>" alt="Hero Image" class="img-fluid rounded-circle" style="border: 5px solid #fff; object-fit: cover; height: 356px; width: 356px;">
                         </div>
                     </div>
                 </div>
